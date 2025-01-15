8499e5b60ac69215b873e30fd7534955513cdaba
Gupples <gupples224@gmail.com>
2025-01-15 16:14:04 -0700

    Updated file. No longer uses name, includes express for statics.

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 // Import express using ESM syntax
 import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+
+// Create __dirname and __filename variables
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Create an instance of an Express application
 const app = express();
 
-// Get your name from .env
-const name = process.env.NAME;
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, "public")));
 
-// Define a route handler for the root URL ('/')
-app.get('/', (req, res) => {
-    res.send(`Hello, ${name}!`);
+// Create static file paths
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+});
+
+app.get("/page1", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/page1.html"))
+});
+
+app.get("/page2", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/page2.html"))
 });
 
 // Define the port number the server will listen on
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
