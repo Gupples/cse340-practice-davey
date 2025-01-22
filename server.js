e07eb49c9ffffc6603c8c9b094d600153ccd4dca
Gupples <gupples224@gmail.com>
2025-01-22 16:28:37 -0700

    Finished setting up live reloading

--- server.js
+++ server.js
@@ -20,26 +20,55 @@ app.set('view engine', 'ejs');
 // Set the views directory (where your templates are stored)
 app.set('views', path.join(__dirname, 'views'));
 
+// When in development mode, start a WebSocket server for live reloading
+if (MODE.includes('dev')) {
+    const ws = await import('ws');
+
+    try {
+        const wsPort = parseInt(PORT) + 1;
+        const wsServer = new ws.WebSocketServer({port: wsPort });
+
+        wsServer.on('listening', () => {
+            console.log(`WebSocket server is running on port ${wsPort}`);
+        });
+
+        wsServer.on('error', () => {
+            console.error('WebSocket server error:', error);
+        });
+    } catch (error) {
+        console.error('Failed to start WebSocket server', error);
+    }
+}
+
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, "public")));
 
 // Create static file paths
+// Home page
 app.get("/", (req, res) => {
     const title = 'Home Page';
     const content = '<h1>Welcome to the Home Page</h1>';
-    res.render('index', { title, content });
+    const mode = process.env.MODE;
+    const port = process.env.PORT;
+    res.render('index', { title, content, mode, port });
 });
 
-app.get("/page1", (req, res) => {
+// About page
+app.get("/about", (req, res) => {
     const title = 'Page 1';
     const content = '<h1>Page 1</h1>';
-    res.render('index', { title, content });
+    const mode = process.env.MODE;
+    const port = process.env.PORT;
+    res.render('index', { title, content, mode, port });
 });
 
-app.get("/page2", (req, res) => {
+// Contact page
+app.get("/contact", (req, res) => {
     const title = 'Page 2';
     const content = '<h1>Page 2</h1>';
-    res.render('index', { title, content });
+    const mode = process.env.MODE;
+    const port = process.env.PORT;
+    res.render('index', { title, content, mode, port });
 });
 
 app.get("/hello", (req, res) => {
@@ -49,7 +78,12 @@ app.get("/hello", (req, res) => {
 // Account page
 app.get('/explore/:name/:age/:id', (req, res) => {
     const { name, age, id } = req.params;
-    res.send('Check your computer console!');
+    const content = `
+        <h1>Hello, <%- name%>! Welcome to the site!</h1>
+        <p>Your age is: <%- age%><br>
+        Your id is: <%- id%></p>
+    `;
+    res.render('index', {title, content, mode, port });
 });
 
 // Start the server and listen on the specified port
