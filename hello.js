1d93cf5e7e842f12c3c126ccae9ae8d4dd81e3d2
Gupples <gupples224@gmail.com>
2025-01-22 15:59:02 -0700

    Finished setting up ejs capability

--- hello.js
+++ hello.js
@@ -1,2 +1,14 @@
+// IMPORT EXPRESS
+import express from "express";
+import {filepath} from "url";
+import path from "path";
+
+const __filename = fileURLtoPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // For output to the terminal.
 console.log(`Hello ${process.env.NAME}`);
+
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+
--- server.js
+++ server.js
@@ -7,27 +7,50 @@ import path from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Define important variables
+const MODE = process.env.MODE || 'production';
+const PORT = process.env.PORT || 3000;
+
 // Create an instance of an Express application
 const app = express();
+
+// Set view engine to ejs
+app.set('view engine', 'ejs');
+
+// Set the views directory (where your templates are stored)
+app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, "public")));
 
 // Create static file paths
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/home.html"))
+    const title = 'Home Page';
+    const content = '<h1>Welcome to the Home Page</h1>';
+    res.render('index', { title, content });
 });
 
 app.get("/page1", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/page1.html"))
+    const title = 'Page 1';
+    const content = '<h1>Page 1</h1>';
+    res.render('index', { title, content });
 });
 
 app.get("/page2", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/page2.html"))
+    const title = 'Page 2';
+    const content = '<h1>Page 2</h1>';
+    res.render('index', { title, content });
 });
 
-// Define the port number the server will listen on
-const PORT = process.env.PORT;
+app.get("/hello", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/hello.ejs"))
+});
+
+// Account page
+app.get('/explore/:name/:age/:id', (req, res) => {
+    const { name, age, id } = req.params;
+    res.send('Check your computer console!');
+});
 
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
