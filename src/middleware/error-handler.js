a5804978f79949af044e224b297173be0da72376
Gupples <gupples224@gmail.com>
2025-02-14 15:50:51 -0700

    Added band-aid fix for missing 'nav' err.

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -53,6 +53,8 @@ export const notFoundHandler = (req, res, next) => {
  * @param {Function} next - The next middleware function in the stack.
  */
 export const globalErrorHandler = (err, req, res, next) => {
+    // Band-aid fix; will be taken care of in a later version.
+    res.locals.nav = ''; 
     // Ensure we are in an error state
     if (!res.locals.isInErrorState) {
         res.locals.isInErrorState = true;
